4eec651b1ea6bacc9d0bf90a2925a307bd2c29da
Alexander <tarassov.al@gmail.com>
2019-01-10 01:45:53 +0300

    login ready

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UsersController < ApiController
   skip_before_action :authenticate_request, :only => [:register, :validate_new_email]
 
   def register
