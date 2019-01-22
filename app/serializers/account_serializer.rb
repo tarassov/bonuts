59225f6703a4eed1a6f2907e449ae49395c06f5d
Alex T <tarassov.al@gmail.com>
2019-01-22 13:31:22 +0300

    serizliz e cache ??

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -2,4 +2,6 @@ class AccountSerializer
   include FastJsonapi::ObjectSerializer
   set_type :account
   attributes :balance, :id
+
+  cache_options enabled: true, cache_length: 2.hours
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,6 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   set_type :user
   attributes :name, :id, :email,:last_name,:first_name,:sex,:notes, :self_account, :distrib_account
+
+  cache_options enabled: true, cache_length: 2.hours
 end
