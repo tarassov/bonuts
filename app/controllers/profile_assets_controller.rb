89c85dca02727f2f4c9f2a60ede1a2cd3b6367f0
Alexander Tarasov <tarassov.al@gmail.com>
2019-05-19 19:58:39 +0300

    assets controller index and update

--- app/controllers/profile_assets_controller.rb
+++ app/controllers/profile_assets_controller.rb
@@ -1,4 +1,5 @@
 class ProfileAssetsController < ApiController
+  before_action :set_asset, only[:update]
   include Ability
 
   def create
@@ -8,13 +9,30 @@ class ProfileAssetsController < ApiController
       profile_id  = asset_params.fetch(:profile_id, @current_profile.id)
       create_profile_asset = CreateProfileAsset.call({profile_id: profile_id, donut_id: donut_id})
       if create_profile_asset.success?
-        @asset = create_profile_asset.result
-        json_response(ProfileAssetSerializer.new(@asset,{}).serialized_json, :created, @asset, :bad_request)
+        asset = create_profile_asset.result
+        json_response(ProfileAssetSerializer.new(asset,{}).serialized_json, :created, asset, :bad_request)
       else
         render_error :forbiden, create_profile_asset.errors[:error].first  
       end
     else
       render_error :forbiden, "Запрещено для этого пространства"        
+    end
+  end
+
+  def index
+    if check_admin || check_profile(asset_params[:profile_id])    
+      profile_assets = ProfileAsset.by_profile(asset_params[:profile_id])
+      json_response(ProfileAssetSerializer.new(profile_assets,{}).serialized_json, :ok)
+    else
+      render_error :forbiden, "Недостаточно полномочий"
+    end
+  end
+
+  def update
+    if check_admin || check_profile(@asset.profile_id)
+      @asset.update(asset_params)  
+    else
+      render_error :forbiden, "Недостаточно полномочий"
     end
   end
 
--- app/models/profile_asset.rb
+++ app/models/profile_asset.rb
@@ -1,4 +1,10 @@
 class ProfileAsset < ApplicationRecord
   belongs_to :profile
   belongs_to :donut
+
+  scope :by_profile, ->(profile_id) {
+    where(profile_id: profile_id)
+  }
+
+  
 end
