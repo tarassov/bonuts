2cbb9a0368c2e8284fe8ae981a56d3a187aad585
Alexander T <tarassov.al@gmail.com>
2019-07-16 09:39:53 +0300

    ?

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require "rails"
 # Pick the frameworks you want:
+require 'carrierwave'
+require 'carrierwave/orm/activerecord'
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -11,6 +13,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,3 @@
-require File.expand_path('../application', __FILE__)
-require 'carrierwave/orm/activerecord'
 # Load the Rails application.
 require_relative 'application'
 
