817c8e48d9257ddecb68dd1c50e0a3490e4d9b75
Alexander Tarasov <tarassov.al@gmail.com>
2020-06-18 15:41:36 +0300

    daily job log_entry check

--- app/logic/operations/service/daily_job.rb
+++ app/logic/operations/service/daily_job.rb
@@ -6,15 +6,18 @@ class DailyJob
 
         schedulers.each do |scheduler|
             ActiveRecord::Base.transaction do
-                operation = ShareAll.call({
-                    tenant: scheduler.tenant,
-                    profile: scheduler.profile,
-                    amount: scheduler.amount,
-                    comment: scheduler.comment,
-                    burn_old: scheduler.burn_old
-                })
+                log_entry = SchedulerLog.where(tenant: scheduler.tenant, donuts_scheduler: scheduler, scheduler_success: true, created_at.to_date:  DateTime.now.to_date)
+                unless log_entry do
+                    operation = ShareAll.call({
+                        tenant: scheduler.tenant,
+                        profile: scheduler.profile,
+                        amount: scheduler.amount,
+                        comment: scheduler.comment,
+                        burn_old: scheduler.burn_old
+                    })
 
-                SchedulerLog.create({tenant: scheduler.tenant, donuts_scheduler: scheduler, error_message: "",scheduler_success: operation.success?})
+                     SchedulerLog.create({tenant: scheduler.tenant, donuts_scheduler: scheduler, error_message: "",scheduler_success: operation.success?})
+                end 
             end
         end
     end    
