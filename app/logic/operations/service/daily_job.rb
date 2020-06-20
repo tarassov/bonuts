27efaba7b401826b850c58c9bb5ccba9d9782ba9
Alexander Tarasov <tarassov.al@gmail.com>
2020-06-21 00:49:09 +0300

    daily job test

--- app/logic/operations/service/daily_job.rb
+++ app/logic/operations/service/daily_job.rb
@@ -1,13 +1,13 @@
 class DailyJob
     def self.call
         t = Time.now
-
         schedulers   = DonutsScheduler.where(day: t.day, active: true, every: 'daily')
 
         schedulers.each do |scheduler|
             ActiveRecord::Base.transaction do
-                log_entry = SchedulerLog.where(tenant: scheduler.tenant, donuts_scheduler: scheduler, scheduler_success: true, created_at.to_date:  DateTime.now.to_date)
-                unless log_entry do
+                date  = DateTime.now
+                log_entry = SchedulerLog.where(tenant: scheduler.tenant, donuts_scheduler: scheduler, scheduler_success: true).where('created_at >= ? and created_at <=?', date.midnight, date.end_of_day)
+                if log_entry.count == 0 
                     operation = ShareAll.call({
                         tenant: scheduler.tenant,
                         profile: scheduler.profile,
@@ -16,8 +16,8 @@ class DailyJob
                         burn_old: scheduler.burn_old
                     })
 
-                     SchedulerLog.create({tenant: scheduler.tenant, donuts_scheduler: scheduler, error_message: "",scheduler_success: operation.success?})
-                end 
+                    SchedulerLog.create({tenant: scheduler.tenant, donuts_scheduler: scheduler, error_message: "",scheduler_success: operation.success?})
+                end            
             end
         end
     end    
