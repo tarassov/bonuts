1b7544d535106b9133cc872a8d34409d9364e8f0
Alexander Tarasov <tarassov.al@gmail.com>
2020-07-22 14:14:35 +0300

    do not send emails for demo tenants

--- app/logic/notifiers/transport/email_transport.rb
+++ app/logic/notifiers/transport/email_transport.rb
@@ -2,14 +2,16 @@
 
 class EmailTransport < TransportBase
   def do_send(notifier)
-    notifier.get_addresses.each do |email|
-      NotifyMailer.notification({
-                                  email: email,
-                                  main_text: notifier.get_main_text,
-                                  title: notifier.get_title,
-                                  subject: notifier.get_subject,
-                                  footer: notifier.get_footer
-                                }).deliver_later
-    end
+    unless @args[:tenant].demo 
+      notifier.get_addresses.each do |email|
+        NotifyMailer.notification({
+                                    email: email,
+                                    main_text: notifier.get_main_text,
+                                    title: notifier.get_title,
+                                    subject: notifier.get_subject,
+                                    footer: notifier.get_footer
+                                  }).deliver_later
+      end
+    end  
   end
 end
