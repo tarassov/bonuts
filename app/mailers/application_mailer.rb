fc065213778d9b3279b5b8ee067d652fecdeef21
Alexander Tarasov <tarassov.al@gmail.com>
2020-07-22 14:07:08 +0300

    from mailer name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'donuts@cki.com.ru'
+  default from: 'Портал пончики <info@bonuts.ru>'
   layout 'mailer'
 end
