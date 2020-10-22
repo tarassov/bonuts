13b5be06b95a9cc38d61e9bbdc88fafd3d5b5d5a
Alexander Tarasov <tarassov.al@gmail.com>
2020-10-22 22:00:56 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_10_13_182429) do
     t.datetime "updated_at"
     t.bigint "deal_id"
     t.index ["account_id"], name: "index_account_operations_on_account_id"
-    t.index ["deal_id"], name: "index_account_operations_on_transaction_id"
+    t.index ["deal_id"], name: "index_account_operations_on_deal_id"
     t.index ["parent_operation_id"], name: "index_account_operations_on_parent_operation_id"
   end
 
@@ -44,8 +44,8 @@ ActiveRecord::Schema.define(version: 2020_10_13_182429) do
     t.bigint "resource_id"
     t.string "author_type"
     t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
@@ -74,12 +74,12 @@ ActiveRecord::Schema.define(version: 2020_10_13_182429) do
     t.index ["profile_id"], name: "index_comments_on_profile_id"
   end
 
-  create_table "deals", id: :bigint, default: -> { "nextval('transactions_id_seq'::regclass)" }, force: :cascade do |t|
+  create_table "deals", force: :cascade do |t|
     t.string "comment"
     t.bigint "profile_id"
     t.datetime "created_at"
     t.string "deal_type"
-    t.index ["profile_id"], name: "index_transactions_on_profile_id"
+    t.index ["profile_id"], name: "index_deals_on_profile_id"
   end
 
   create_table "departments", force: :cascade do |t|
