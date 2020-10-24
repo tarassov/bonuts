156b649dd8bead0da9f08198c04121200e115553
Alexander Tarasov <tarassov.al@gmail.com>
2020-10-24 21:13:24 +0300

    test fix

--- spec/logic/update_tenant_plugin_spec.rb
+++ spec/logic/update_tenant_plugin_spec.rb
@@ -22,7 +22,7 @@ describe UpdateTenantPlugin do
   context 'when user not admin' do
     before do
       @profileNotAdmin = @tenant.profiles.where(:admin => false)[0]
-      @resultNotAdmin = UpdateTenantPlugin.call({plugin_id: @tenant_plugin.id, profile: @profileNotAdmin, active: true, tenant_settings:  @tenant_settings})
+      @resultNotAdmin = UpdateTenantPlugin.call({plugin_id: @plugin.id, profile: @profileNotAdmin, active: true, tenant_settings:  @tenant_settings})
     end
     it 'fails' do
       expect(@resultNotAdmin.errors.count).to eq  1
@@ -31,7 +31,7 @@ describe UpdateTenantPlugin do
 
   context 'when success' do
       before do
-        @resultNotAdmin = UpdateTenantPlugin.call({plugin_id: @tenant_plugin.id, profile: @profileAdmin, active: true, tenant_settings:  @tenant_settings})
+        @resultNotAdmin = UpdateTenantPlugin.call({plugin_id: @plugin.id, profile: @profileAdmin, active: true, tenant_settings:  @tenant_settings})
       end
 
       it 'updates host to: "demo.ru"' do
