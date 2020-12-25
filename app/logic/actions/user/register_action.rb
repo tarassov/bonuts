0922d971b6ad28ddfa04602df853a9c033c8472f
Alexander Tarasov <tarassov.al@gmail.com>
2020-12-26 00:32:58 +0300

    added register logic operation. notifier spec fails

---
+++ app/logic/actions/user/register_action.rb
@@ -0,0 +1,25 @@
+class RegisterAction < BaseAction
+   
+    protected
+    def do_call 
+        user_count = User.where(email: @args[:email]).count
+        if user_count>0
+            errors.add :error, I18n.t('email_taken')   
+            return nil
+        end
+        @user = User.create!({email: @args[:email], password: @args[:email], first_name: @args[:first_name], last_name:  @args[:last_name]})
+        tenant = @args[:tenant]
+        profile = Profile.new({ tenant_id: tenant.id, default: true, active: true })
+        profile.save
+        @user.demo = tenant.demo
+        @user.email_confirmed = tenant.demo
+        @user.save
+        @user.profiles << profile
+        log = PublicEventAction.call({ profile: profile, content: @user.name + ' присоединился(лась) к проекту' })
+        @event = log.result
+      
+        return @user 
+    end
+end
+  
+   
---
+++ app/logic/operations/user/register.rb
@@ -0,0 +1,23 @@
+class Register <  BaseOperation
+
+  def check_args args
+    @profile = nil
+
+    @tenant = args.fetch(:tenant, nil)
+    raise "Tenant argument should be passed to create register operation " + self.class.name  unless @tenant 
+
+  end
+
+  def do_call
+      @action = @action_factory.register  @args
+      notifier = NewUserNotifier.new @args
+      notifier.add_transport(LoggerTransport.new)
+      notifier.add_transport(UnifiedTransport.new)
+      #@action.attach_validator(AdminValidator.new(@args))
+      @action.call
+    end
+  
+    def operation_result
+      @action.action_result
+  end
+end
