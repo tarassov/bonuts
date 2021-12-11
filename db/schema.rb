0fc271b1a701368659f8f7b485c44d125325d93f
Alexander Tarasov <tarassov.al@gmail.com>
2021-12-11 17:48:21 +0300

    schema bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_11_29_203949) do
     t.datetime "updated_at"
     t.bigint "deal_id"
     t.index ["account_id"], name: "index_account_operations_on_account_id"
-    t.index ["deal_id"], name: "index_account_operations_on_transaction_id"
+    t.index ["deal_id"], name: "index_account_operations_on_deal_id"
     t.index ["parent_operation_id"], name: "index_account_operations_on_parent_operation_id"
   end
 
@@ -35,32 +35,6 @@ ActiveRecord::Schema.define(version: 2021_11_29_203949) do
     t.bigint "profile_id"
     t.index ["profile_id"], name: "index_accounts_on_profile_id"
     t.index ["tenant_id"], name: "index_accounts_on_tenant_id"
-  end
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
@@ -74,12 +48,12 @@ ActiveRecord::Schema.define(version: 2021_11_29_203949) do
     t.index ["profile_id"], name: "index_comments_on_profile_id"
   end
 
-  create_table "deals", id: :bigint, default: -> { "nextval('transactions_id_seq'::regclass)" }, force: :cascade do |t|
+  create_table "deals", force: :cascade do |t|
     t.string "comment"
     t.bigint "profile_id"
     t.datetime "created_at"
     t.string "deal_type"
-    t.index ["profile_id"], name: "index_transactions_on_profile_id"
+    t.index ["profile_id"], name: "index_deals_on_profile_id"
   end
 
   create_table "departments", force: :cascade do |t|
@@ -96,8 +70,8 @@ ActiveRecord::Schema.define(version: 2021_11_29_203949) do
     t.datetime "expiration_date"
     t.string "name"
     t.boolean "active"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.bigint "profile_id"
     t.string "logo"
     t.string "description"
