609e1246e25f1ed9222473bfe87edb281691d74d
Alexander Tarasov <tarassov.al@gmail.com>
2021-12-11 18:37:52 +0300

    create donut ability fix

--- app/logic/additions/abilities/donut_ability.rb
+++ app/logic/additions/abilities/donut_ability.rb
@@ -8,6 +8,11 @@ class DonutAbility < BaseAbility
       can :create, Donut
     end
 
+    def store_admin_abilities profile
+      can :manage, Donut, tenant: profile.tenant
+      can :create, Donut
+    end
+
     def user_abilities(profile)
         can :read, Donut, tenant: profile.tenant
     end
--- spec/logic/donut/create_donut_spec.rb
+++ spec/logic/donut/create_donut_spec.rb
@@ -4,25 +4,60 @@ describe  CreateDonut do
   before(:context) do
     @tenant = create(:tenant_with_profiles)         
     @profileAdmin = @tenant.profiles.where(:admin => true)[0]    
-    @profileUser = @tenant.profiles.where(:admin => false)[0]            
+    @profileUser = @tenant.profiles.where(:admin => false)[0]          
+    @profileStoreAdmin = @tenant.profiles.where(:admin => false)[1]  
+    @profileStoreAdmin.store_admin = true
+    @profileStoreAdmin.save
   end
 
-  context 'when success' do
+  context 'when admin -  success' do
     before do
-      @result_success =  CreateDonut.call({profile: @profileAdmin}) 
+      @result_success =  CreateDonut.call({profile: @profileAdmin, name: 'NewDonut', price: 10}) 
     end
-    it ' do smth '
+    it ' creates new donut ' do
+        expect(Donut.where( name: 'NewDonut', price: 10, tenant: @tenant).count).to eq 1
+    end
 
-    it 'does not return error'do
-      expect(@result_success.errors.count).to eq(0), @result_success.errors[:error].join(', ')
+    it 'does not return error' do
+      if @result_success.errors.count > 0
+        errors = @result_success.errors[:error].nil? ? '' : @result_success.errors[:error].join(', ')
+        forbidden = @result_success.errors[:forbidden].nil? ? '' : @result_success.errors[:forbidden].join(', ')
+        message = errors+forbidden
+      else
+        message=''
+      end
+      expect(@result_success.errors.count).to eq(0), message
+    end
+  end
+
+  
+  context 'when store admin -  success' do
+    before do
+      @result_success =  CreateDonut.call({profile: @profileStoreAdmin, name: 'NewDonutS', price: 10}) 
+    end
+    it ' creates new donut ' do
+        expect(Donut.where( name: 'NewDonutS', price: 10, tenant: @tenant).count).to eq 1
+    end
+
+    it 'does not return error' do
+      if @result_success.errors.count > 0
+        errors = @result_success.errors[:error].nil? ? '' : @result_success.errors[:error].join(', ')
+        forbidden = @result_success.errors[:forbidden].nil? ? '' : @result_success.errors[:forbidden].join(', ')
+        message = errors+forbidden
+      else
+        message=''
+      end
+      expect(@result_success.errors.count).to eq(0), message
     end
   end
 
   context 'when fails' do
     before do
-      @result_fail = CreateDonut.call({profile: @profileUser}) 
+      @result_fail = CreateDonut.call({profile: @profileUser,name: 'WrongDonut', price: 20}) 
     end
-       
+    it ' does not create new donut ' do
+      expect(Donut.where( name: 'WrongDonut', price: 20, tenant: @tenant).count).to eq 0
+    end
     it 'returns error'do
       expect(@result_fail.errors.count).to eq 1
     end
