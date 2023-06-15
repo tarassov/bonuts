f8fcb80032e00609a514d317024b5cc99aef9840
Alexander Tarasov <tarassov.al@gmai.com>
2023-06-16 02:38:19 +0300

    post new donut

--- app/logic/actions/donut/create_donut_action.rb
+++ app/logic/actions/donut/create_donut_action.rb
@@ -1,25 +1,24 @@
 class CreateDonutAction < BaseAction
-    def result_event
-        @donut
-    end
+  def result_event
+    @donut
+  end
 
-    def args_to_check
-        %i[tenant profile price name]
-    end
+  def args_to_check
+    %i[tenant profile price name]
+  end
 
-    protected
-    def do_call 
-        @donut = Donut.create!({
-            active: @args.fetch(:active, nil), 
-            name: @args.fetch(:name, nil), 
-            tenant: @args[:tenant], 
-            price: @args[:price], 
-            profile: @args[:profile], 
-            expiration_date: @args.fetch(:description, nil), 
-            description: @args.fetch(:description, nil), 
-            logo:  @args.fetch(:logo, nil)
-            })
-    end
+  protected
+
+  def do_call
+    @donut = Donut.create!({
+                             active: @args.fetch(:active, nil),
+                             name: @args.fetch(:name, nil),
+                             tenant: @args[:tenant],
+                             price: @args[:price],
+                             profile: @args[:profile],
+                             expiration_date: @args.fetch(:description, nil),
+                             description: @args.fetch(:description, ''),
+                             logo: @args.fetch(:logo, nil)
+                           })
+  end
 end
-  
-   
--- spec/requests/api/donuts_spec.rb
+++ spec/requests/api/donuts_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'api/v1/donuts_controller', type: :request do
   before(:context) do
     @tenant = create(:tenant_with_profiles)
     @donuts = create_list(:donut, 10, tenant: @tenant)
+    @store_admin = create(:profile, tenant: @tenant, store_admin: true)
   end
   path '/donuts' do
     get 'get all active donuts' do
@@ -32,7 +33,51 @@ RSpec.describe 'api/v1/donuts_controller', type: :request do
         run_test!
       end
     end
+    post 'create new donut' do
+      tags 'Donuts'
+      consumes 'multipart/form-data'
+      produces 'application/json'
+      parameter name: :FormData, in: :formData, type: :object, required: true, schema:
+        {
+          "type": 'object',
+          "required": %w[
+            tenant
+            logo
+            name
+            price
+          ],
+          "properties": {
+            "price": { "type": 'number' },
+            "name": { "type": 'string' },
+            "tenant": { "type": 'string' },
+            "logo": { "type": 'file' }
+          }
+
+        }
+      parameter name: :price, in: :formData, type: :number, required: true
+      parameter name: :name, in: :formData, type: :string, required: true
+      parameter name: :tenant, in: :formData, type: :string, required: true
+      parameter name: :logo, in: :formData, type: :file, required: true
+      security [{ bearer_auth: [] }]
+      expected_response_schema = SpecSchemas::Donut.response
+      response '200', 'success' do
+        let(:FormData) { { logo: FactoryBot.attributes_for(:image), id: 10, tenant: @tenant.name, price: 10, name: 'test' } }
+        let(:price) { 10 }
+        let(:name) { 'test' }
+        let(:tenant) { @tenant.name }
+        let(:logo) { FactoryBot.attributes_for(:image) }
+        let(:Authorization) { "Bearer #{JsonWebToken.encode(user_id: @store_admin.user.id)}" }
+
+        before do |example|
+          submit_request(example.metadata)
+        end
+
+        schema expected_response_schema
+        run_test!
+      end
+    end
   end
+
   path '/donuts/{id}' do
     get 'get donut' do
       tags 'Donuts'
