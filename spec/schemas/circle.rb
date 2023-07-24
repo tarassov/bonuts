4d318db26d399f5563285d3a6adefde656885134
Alexander Tarasov <tarassov.al@gmai.com>
2023-07-24 16:56:56 +0300

    edit circle fix

--- spec/schemas/circle.rb
+++ spec/schemas/circle.rb
@@ -23,10 +23,7 @@ module SpecSchemas
         "type": 'object',
 
         "properties": {
-          "data": {
-            "type": 'object',
-            "items": SpecSchemas::Circle.schema
-          }
+          "data": SpecSchemas::Circle.schema
         }
       }
     end
