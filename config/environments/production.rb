68c31d9561aef9051320c73f71b77cf4094a197d
Alexander Tarasov <tarassov.al@gmai.com>
2023-11-06 23:44:58 +0300

    fix mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = { from: 'Портал пончики <postmaster@probonuts.ru>' }
-  config.action_mailer.default_url_options = { host: 'https://new.bonuts.ru' }
+  config.action_mailer.default_url_options = { host: 'https://bonuts.ru' }
 
   config.action_mailer.delivery_method = :smtp
 
