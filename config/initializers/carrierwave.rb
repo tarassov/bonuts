38154b5f0313a4441cfd7ed15cffef03b08289dd
Alexander Tarasov <tarassov.al@gmai.com>
2023-11-26 18:42:34 +0300

    staging deploy (fix CarrierWave)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,6 @@ CarrierWave.configure do |config|
   config.asset_host = if Rails.env.development?
                         Rails.application.config.action_mailer.default_url_options[:host]
                       else
-                        Rails.configuration.bonuts_config['bonuts_config'] || 'https://api.bonuts.ru'
+                        Rails.configuration.bonuts_config['api'] || 'https://api.bonuts.ru'
                       end
 end
