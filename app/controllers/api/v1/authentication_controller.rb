0ab83473ba43dbfbd102cc0fed71c3c4d5cc4cb5
Alexander Tarasov <tarassov.al@gmai.com>
2024-04-07 21:54:21 +0300

    logout fix

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -10,25 +10,34 @@ class Api::V1::AuthenticationController < Api::V1::ApiController
     end
     auth_token = JsonWebToken.encode(user_id: @current_user.id)
     cookies.signed[:jwt] =
-      { value: auth_token, httponly: true, same_site: 'None', secure: true, domain: :all }
-    render json: { tenants:, username: @current_user.email,
-                   auth_token:, currentTenant: @current_tenant }
+      { value: auth_token, httponly: true, same_site: "None", secure: true, domain: :all }
+    render(json: {
+      tenants:,
+      username: @current_user.email,
+      auth_token:,
+      currentTenant: @current_tenant,
+    })
     # render json: { tenants: tenants, currentTenant: params[:current_tenant], username:  @current_user.email, auth_token: JsonWebToken.encode(user_id:  @current_user.id) }
   end
 
   def logout
-    cookies.delete(:jwt)
     cookies.delete(:jwt, domain: :all)
-    render json: { status: 'ok' }, status: :ok
+    render(json: { status: "ok" }, status: :ok)
   end
 
   def authenticate
     command = AuthenticateUser.call(params[:email].downcase, params[:password], params[:current_tenant])
     if command.success?
       cookies.signed[:jwt] =
-        { value: command.result[:auth_token], httponly: true, same_site: :none,
-          secure: true, domain: :all, expires: 1.year }
-      render json: command.result
+        {
+          value: command.result[:auth_token],
+          httponly: true,
+          same_site: :none,
+          secure: true,
+          domain: :all,
+          expires: 1.year,
+        }
+      render(json: command.result)
     else
       # render_error :forbidden, command.errors[:user_authentication].first
       error = command.errors[:user_authentication].first
@@ -36,8 +45,10 @@ class Api::V1::AuthenticationController < Api::V1::ApiController
       errorCode = error.fetch(:errorCode, 0)
       errorParams = error.fetch(:errorParams, {})
 
-      render json: { error: true, message: errorMessage, errorText: errorMessage, errorCode:, errorParams: },
-             status: :forbidden
+      render(
+        json: { error: true, message: errorMessage, errorText: errorMessage, errorCode:, errorParams: },
+        status: :forbidden,
+      )
       # render_error :forbidden, command.errors[:user_authentication].first
     end
   end
@@ -46,11 +57,17 @@ class Api::V1::AuthenticationController < Api::V1::ApiController
     command = DemoAuthenticateUser.call(params[:current_tenant])
     if command.success?
       cookies.signed[:jwt] =
-        { value: command.result[:auth_token], httponly: true, same_site: :none,
-          secure: true, domain: :all, expires: 1.year }
-      render json: command.result
+        {
+          value: command.result[:auth_token],
+          httponly: true,
+          same_site: :none,
+          secure: true,
+          domain: :all,
+          expires: 1.year,
+        }
+      render(json: command.result)
     else
-      render_error :forbidden, command.errors[:user_authentication].first
+      render_error(:forbidden, command.errors[:user_authentication].first)
     end
   end
 end
