845206e26469f58fb03e780dbb469ee62e96e59c
Alexander Tarasov <tarassov.al@gmai.com>
2024-04-08 19:39:59 +0300

    hotfix/dev-184 create invitation error when checking email

--- app/logic/actions/invitation/create_invitation_action.rb
+++ app/logic/actions/invitation/create_invitation_action.rb
@@ -6,7 +6,7 @@ class CreateInvitationAction < BaseAction
   end
 
   def args_to_check
-    %i[email first_name last_name]
+    [:email, :first_name, :last_name]
   end
 
   protected
@@ -14,22 +14,33 @@ class CreateInvitationAction < BaseAction
   def do_call
     @demo = tenant.demo
     # find user if user exists
-    @user = User.where(email: @args[:email]).first
+    @user = User.where(email: @args[:email].downcase).first
 
     # check in any invitation exsts
     if @user && Invitation.exists?(user: @user, tenant:, activated: false, closed: false)
-      errors.add :error, I18n.t('invitation.invitation_exists')
+      errors.add(:error, I18n.t("invitation.invitation_exists"))
       return @user
     end
 
     # create new user if no user exists
-    @user ||= User.create!({ email: @args[:email].downcase, password: User.generate_password,
-                             first_name: @args[:first_name].capitalize, last_name: @args[:last_name].capitalize, active: true, email_confirmed: @demo })
+    @user ||= User.create!({
+      email: @args[:email].downcase,
+      password: User.generate_password,
+      first_name: @args[:first_name].capitalize,
+      last_name: @args[:last_name].capitalize,
+      active: true,
+      email_confirmed: @demo,
+    })
     @user.set_recover_token
 
     # create invitation
-    @invitation = Invitation.create!({ user: @user, tenant:, from_user: profile.user,
-                                       activated: false, closed: false })
+    @invitation = Invitation.create!({
+      user: @user,
+      tenant:,
+      from_user: profile.user,
+      activated: false,
+      closed: false,
+    })
 
     # add deal to invitation entity stack
     @invitation.deals << action_deal(:new_invitation)
