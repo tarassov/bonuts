0711310bf43f8f2ab4e6ad6c445e25405b967951
Alexander Tarasov <tarassov.al@gmai.com>
2024-10-01 15:39:53 +0300

    code style

--- app/serializers/tenant_serializer.rb
+++ app/serializers/tenant_serializer.rb
@@ -5,11 +5,26 @@ class TenantSerializer
   set_type :tenant
   set_id :id
   attributes :id,
-             :name,
-             :caption, :created_at, :updated_at,
-             :logo, :domain, :demo, :active, :test, :welcome_points, :welcome_donuts,
-             :join_to_project_donuts, :birthday_donuts, :join_to_company_donuts, :join_to_company_points, :birthday_points,
-             :join_to_project_points, :use_departments, :email_notification, :birthday_message
+    :name,
+    :caption,
+    :created_at,
+    :updated_at,
+    :logo,
+    :domain,
+    :demo,
+    :active,
+    :test,
+    :welcome_points,
+    :welcome_donuts,
+    :join_to_project_donuts,
+    :birthday_donuts,
+    :join_to_company_donuts,
+    :join_to_company_points,
+    :birthday_points,
+    :join_to_project_points,
+    :use_departments,
+    :email_notification,
+    :birthday_message
 
   attribute :attached do |tenant, params|
     tenant.user_attached(params[:user]) if params[:user]
