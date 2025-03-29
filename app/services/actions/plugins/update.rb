ddb9a30cb3989b786ca1511ac64a7ec5250b255e
Alexander Tarasov <tarassov.al@gmail.com>
2025-03-30 02:50:33 +0300

    feat/dev-46-2  refactor

--- app/services/actions/plugins/update.rb
+++ app/services/actions/plugins/update.rb
@@ -1,39 +1,44 @@
 # frozen_string_literal: true
-
 class Actions::Plugins::Update < BaseAction
-  def args_to_check
+  def required_arguments
     [:tenant_plugin, :plugin_settings]
   end
 
   protected
 
   def do_call
-    # check existence
     tenant_plugin = get_argument(:tenant_plugin)
     plugin_settings = get_argument(:plugin_settings)
 
-    unless tenant_plugin
-      errors.add(:error, I18n.t("plugin.plugin_not_found"))
-      return
-    end
+    return handle_missing_plugin unless tenant_plugin
 
-    if tenant_plugin.nil?
-      errors.add(:error, I18n.t("plugin.plugin_not_found"))
-      return
-    end
+    plugin_id = tenant_plugin.plugin.id
+    tenant_id = tenant_plugin.tenant.id
 
     plugin_settings.each do |setting|
-      plugin_setting = tenant_plugin.plugin_settings.find_by({ plugin_property_id: setting[:id] })
-      if plugin_setting.nil?
-        plugin_setting = tenant_plugin.plugin_settings.build({ plugin_id: tenant_plugin.plugin.id, tenant_id: tenant_plugin.tenant.id, plugin_property_id: setting[:id], value: setting[:value] })
-      else
-        plugin_setting.value = setting[:value]
-      end
-      plugin_setting.save
+      update_or_create_plugin_setting(tenant_plugin, setting, plugin_id, tenant_id)
     end
 
     tenant_plugin
   end
+
+  private
+
+  def handle_missing_plugin
+    errors.add(:error, I18n.t("plugin.plugin_not_found"))
+    nil
+  end
+
+  def update_or_create_plugin_setting(tenant_plugin, setting, plugin_id, tenant_id)
+    plugin_setting = tenant_plugin.plugin_settings.find_or_initialize_by(plugin_property_id: setting[:id])
+
+    if plugin_setting.new_record?
+      plugin_setting.plugin_id = plugin_id
+      plugin_setting.tenant_id = tenant_id
+      plugin_setting.plugin_property_id = setting[:id]
+    end
+
+    plugin_setting.value = setting[:value]
+    plugin_setting.save
+  end
 end
-
-
